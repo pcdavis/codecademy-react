3af9272ad167788eb47810c6bba8e21fde0410e4
pcdavis <pcdavis@users.noreply.github.com>
2017-10-17 18:13:27 -0400

    work completed at library

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,6 +11,29 @@ class SearchBar extends React.Component{
   function renderSortByOptions(){
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key = {sortByOptionValue}> {sortByOption}</li>
     });
   }
+  render(){
+    return (
+      <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+      <ul>
+      {this.renderSortByOptions()}  // This is probably wrong. I'm supposed to call the method and I'm assuming it retuns li items to poplate the ul
+      </ul>
+      </div>
+      <div className="SearchBar-fields">
+      <input placeholder="Search Businesses" />
+      <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+      <a>Lets Go</a>
+      </div>
+      </div>
+
+    )
+  }
 };
+
+
+export default SearchBar;
