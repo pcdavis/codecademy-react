0cdb917996899e3a2e639b156cc57709f74e3c1a
pcdavis <pcdavis@users.noreply.github.com>
2017-10-31 21:34:45 -0400

    Oct 31 Ravenous II

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-class App extends Component {
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+      business,
+      business,
+      business,
+      business,
+      business,
+      business
+    ];
+      console.log(businesses);
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My first react app</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <h1>ravenous</h1>
+            <SearchBar />
+            <BusinessList businesses = {businesses} />
       </div>
     );
   }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count"
+const sortByOptions= {
+  "Best Match": 'best_match',
+  "Highest Rated": 'rating',
+  "Most Reviewed": 'review_count'
 };
 
-class SearchBar extends React.Component{
-  function renderSortByOptions(){
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-    });
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+                  term: '',
+                  locations: '',
+                  sortBy: 'best_match'
+                };
+  };
+
+
+
+  getSortByClass(sortByOption){
+    if(this.state.sortBy === sortByOption){
+      return 'active';
+    }else {
+      return '';
+    }
   }
-};
+  handleSortByChange(sortByOption){
+
+    this.setState({sortBy: sortByOption})
+  }
+
+  renderSortByOptions(){
+      return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this,sortByOptionValue)}>{sortByOption}</li>
+    }); // end of mapping that produces list of ways to search/filter results
+  } // end of renderSortByOptions method
+
+  render(){
+    return (
+        <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+              <ul>
+                {this.renderSortByOptions()}
+              </ul>
+            </div>
+            <div className="SearchBar-fields">
+              <input placeholder="Search Businesses" />
+              <input placeholder="Where?" />
+            </div>
+          <div className="SearchBar-submit">
+            <a>Lets Go</a>
+          </div>
+        </div> //End main div
+    ); //end of return statement
+  } // end render method
+
+}; // enb of SearchBar component
+
+export default SearchBar;
