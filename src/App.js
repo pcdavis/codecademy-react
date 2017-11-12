de1e7cc359b20e959ccf7126729324067268dbe0
pcdavis <pcdavis@users.noreply.github.com>
2017-11-11 22:39:26 -0500

    11/11 almost done

--- src/App.js
+++ src/App.js
@@ -3,37 +3,24 @@ import './App.css';
 import logo from './logo.svg';
 
 import BusinessList from './components/BusinessList/BusinessList';
+import Business from './components/Business/Business';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-      business,
-      business,
-      business,
-      business,
-      business,
-      business
-    ];
-      console.log(businesses);
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses:[]
+    };//end of states
+    this.mysearchYelp = this.mysearchYelp.bind(this);
+  }
 
   mysearchYelp(term,location,sortBy){
-    console.log("search for "+ term);
-    console.log(term);
-    console.log(location);
-    console.log(sortBy);
+    Yelp.search(term,location,sortBy).then((businesses) => {
+      this.setState({businesses: businesses})
+    })
   }
 
   render() {
@@ -41,7 +28,7 @@ class App extends React.Component {
       <div className="App">
             <h1>ravenous</h1>
             <SearchBar searchYelpProp={this.mysearchYelp} />
-            <BusinessList businesses = {businesses} />
+            <BusinessList businesses = {this.state.businesses} />
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,11 +1,51 @@
 const clientId = 'm61NdXJbPz1nVmzKO0XRfA';
 const secret = 'c7UqBiVbpWW2xfTA9VdYER6fNWdj7akegrb8ZxGHIp5cCzszXNMStsrpcWDB5qNq';
-const accessToken;
+let accessToken
 
 const Yelp = {
   getAccessToken(){
     if(accessToken) {
-      return new Promise(resolve => resolve(accessToken)); //end of new Promise
-    }
-  }
+      return new Promise(resolve => resolve(accessToken));
+    } // end of if conditional
+      return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(response => { return response.json(); }).then( jsonResponse => {
+        accessToken = jsonResponse.access_token;
+      });
+  }, // end of getAccessToken method
+
+  search(term,location,sortBy){
+    return Yelp.getAccessToken().then(() => {
+      return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+        headers: { Authorization: `Bearer ${accessToken}` }
+      }); // end of return fetch
+    }).then((response) => {
+      return response.json()
+    }).then(function(jsonResponse){
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories.title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
+      }
+    })
+  }// end of search
 };
+
+
+export default Yelp;
+
+// Possible alternative to instantly resolving promise from getAccessToken
+// Promise.resolve(accessToken).then(function(myValue) {
+//   console.log(myValue);
+// }, function(myValue){
+//   console.log('Instant promise was not resolved for some reason')
+// });
